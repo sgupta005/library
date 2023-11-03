1f6fb064a9d1ad2c7236cd1414a47ac809084504
Shivam Gupta <shivamgupta02005@gmail.com>
2023-11-03 23:47:14 +0530

    Made the code cleaner and improved styling

--- main.js
+++ main.js
@@ -7,52 +7,96 @@ function Book(title,author,pages,read){
     this.read = read;
 }
 
-const addBookButton = document.getElementById('add-book-button');
-const bookForm = document.getElementById('book-form');
-const addBookForm = document.getElementById('add-book-form');
+const showFormButton = document.getElementById('show-form-button');
+const bookFormDialog = document.getElementById('book-form-dialog');
+const bookFormContainer = document.getElementById('book-form-container');
 const bookGridContainer = document.getElementById('book-grid-container');
+const submitButton = document.getElementById('form-submit-button');
+const closeButton = document.getElementById('form-cancel-button');
 
-addBookButton.addEventListener('click', ()=>{
-    addBookButton.disabled = true;
-    bookForm.style.display = 'flex';
-})
+showFormButton.addEventListener('click', ()=>{
+    bookFormDialog.showModal();
+});
 
-addBookForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-
-    const title = document.getElementById('book-title').value;
-    const author = document.getElementById('book-author').value;
-    const pages = parseInt(document.getElementById('book-pages').value);
-    const read = document.getElementById('book-read').value;
-
-    const book = new Book(title,author,pages,read);
-    myLibrary.push(book);
-    bookCard = createBookCard(book);
-    bookGridContainer.appendChild(bookCard);
-    console.log(myLibrary);
-    addBookForm.reset();
-    bookForm.style.display = 'none';
-    addBookButton.disabled = false;
-})
-
-function createBookCard(book){
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-    const title = document.createElement('p');
-    const author = document.createElement('p');
-    const pages = document.createElement('p');
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = book.pages;
-    bookCard.append(title,author,pages);
-    return bookCard;
-}
-
-document.addEventListener('click', hideBox);
-function hideBox(event){
-    if (!bookForm.contains(event.target) && !addBookButton.contains(event.target)){
-        bookForm.style.display = 'none';
-        addBookButton.disabled = false;
+document.addEventListener('click', (event)=>{
+    if ((!bookFormContainer.contains(event.target) && !showFormButton.contains(event.target)) || closeButton.contains(event.target)){
+        bookFormDialog.close();
     }
-}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const addBookButton = document.getElementById('add-book-button');
+// const bookForm = document.getElementById('book-form');
+// const addBookForm = document.getElementById('add-book-form');
+// const bookGridContainer = document.getElementById('book-grid-container');
+
+// addBookButton.addEventListener('click', ()=>{
+//     addBookButton.disabled = true;
+//     bookForm.style.display = 'flex';
+// })
+
+// addBookForm.addEventListener('submit', (e)=>{
+//     e.preventDefault();
+
+//     const title = document.getElementById('book-title').value;
+//     const author = document.getElementById('book-author').value;
+//     const pages = parseInt(document.getElementById('book-pages').value);
+//     const read = document.getElementById('book-read').value;
+
+//     const book = new Book(title,author,pages,read);
+//     myLibrary.push(book);
+//     bookCard = createBookCard(book);
+//     bookGridContainer.appendChild(bookCard);
+//     console.log(myLibrary);
+//     addBookForm.reset();
+//     bookForm.style.display = 'none';
+//     addBookButton.disabled = false;
+// })
+
+// function createBookCard(book){
+//     const bookCard = document.createElement('div');
+//     bookCard.classList.add('book-card');
+//     const title = document.createElement('p');
+//     const author = document.createElement('p');
+//     const pages = document.createElement('p');
+//     title.textContent = book.title;
+//     author.textContent = book.author;
+//     pages.textContent = book.pages;
+//     bookCard.append(title,author,pages);
+//     return bookCard;
+// }
+
+// 
 
